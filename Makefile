d29304fa0ad338cfbb2279be49c928679e0b0a87
Timothy Palpant <tim@palpant.us>
2017-01-02 12:22:17 -0500

    add gRPC-JSON gateway

--- Makefile
+++ Makefile
@@ -9,7 +9,13 @@ proto:
 		deck/*.proto
 	protoc -I/usr/local/include -I${SRC_DIR} \
 		-I${GOPATH}/src \
-		--go_out=plugins=grpc:${SRC_DIR} \
+		-I${GOPATH}/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+		--go_out=Mgoogle/api/annotations.proto=github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis/google/api,plugins=grpc:${SRC_DIR} \
+		*.proto
+	protoc -I/usr/local/include -I. \
+		-I${GOPATH}/src \
+		-I${GOPATH}/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+		--grpc-gateway_out=logtostderr=true:. \
 		*.proto
 
 server:
--- gameserver/gamed/main.go
+++ gameserver/gamed/main.go
@@ -1,23 +1,42 @@
 // gamed is the main entrypoint for the rummy game server.
+// It runs a gRPC server, as well as a JSON reverse-proxy for that
+// server using gRPC-gateway.
 package main
 
 import (
 	"flag"
 	"fmt"
 	"net"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
 
 	"github.com/golang/glog"
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 
 	"rummy"
 	"rummy/gameserver"
 )
 
+func runProxy(ctx context.Context, grpcEndpoint string, port int) error {
+	mux := runtime.NewServeMux()
+	opts := []grpc.DialOption{grpc.WithInsecure()}
+	err := rummy.RegisterRummyServiceHandlerFromEndpoint(ctx, mux, grpcEndpoint, opts)
+	if err != nil {
+		return err
+	}
+
+	jsonEndpoint := fmt.Sprintf(":%d", port)
+	http.ListenAndServe(jsonEndpoint, mux)
+	return nil
+}
+
 func main() {
 	port := flag.Int("port", 8081, "Port to run gRPC service on")
+	proxyPort := flag.Int("proxyport", 8082, "Port to run JSON proxy on")
 	flag.Parse()
 
 	endpoint := fmt.Sprintf(":%d", *port)
@@ -31,6 +50,12 @@ func main() {
 	rummyServer := gameserver.NewRummyServer(endpoint)
 	rummy.RegisterRummyServiceServer(grpcServer, rummyServer)
 	go grpcServer.Serve(lis)
+
+	glog.Info("Starting JSON proxy")
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+	go runProxy(ctx, endpoint, *proxyPort)
 
 	// Wait for SIGINT and SIGTERM (HIT CTRL-C)
 	ch := make(chan os.Signal)
